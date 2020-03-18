38a5ad883f1a8a957ab23839ad7738645d93f062
Laurence Smith <laurence.smith@airelogic.com>
2020-03-18 12:29:35 +0000

    APM-408 Some very basic terraform config

--- base.tf
+++ base.tf
@@ -1,8 +1,17 @@
 variable "org" {}
 variable "env" {}
 
+
 provider "apigee" {
   org = "${var.org}"
   user = "${var.user}"
   password = "${var.password}"
+}
+
+
+module "personal-demographics-service" {
+  source = "./service.tf"
+
+  name = "personal-demographics"
+  path = "personal-demographics"
 }
