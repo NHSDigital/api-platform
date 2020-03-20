5f5c845e63d61a132eb99786a807d852a8e03c35
Laurence Smith <laurence.smith@airelogic.com>
2020-03-20 11:04:03 +0000

    APM-408 Run formatter

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ variable "identity_url" { default = "" }
 
 
 provider "apigee" {
-  org = var.org
-  user = var.username
+  org      = var.org
+  user     = var.username
   password = var.password
 }
 
@@ -16,20 +16,20 @@ provider "apigee" {
 resource "apigee_target_server" "ig3" {
   count = length(regexall("sandbox", var.env)) > 0 ? 0 : 1
 
-  name = "ig3"
-  host = var.ig3_url
-  env = var.env
+  name    = "ig3"
+  host    = var.ig3_url
+  env     = var.env
   enabled = true
-  port = 443
+  port    = 443
 
   ssl_info {
-    ssl_enabled = true
-    client_auth_enabled = true
-    key_store = "keystore"
-    key_alias = "ig3"
+    ssl_enabled              = true
+    client_auth_enabled      = true
+    key_store                = "keystore"
+    key_alias                = "ig3"
     ignore_validation_errors = false
-    ciphers = []
-    protocols = []
+    ciphers                  = []
+    protocols                = []
   }
 }
 
@@ -37,18 +37,18 @@ resource "apigee_target_server" "ig3" {
 resource "apigee_target_server" "identity-server" {
   count = length(regexall("sandbox", var.env)) > 0 ? 0 : 1
 
-  name = "identity-server"
-  host = var.identity_url
-  env = var.env
+  name    = "identity-server"
+  host    = var.identity_url
+  env     = var.env
   enabled = true
-  port = 443
+  port    = 443
 
   ssl_info {
-    ssl_enabled = true
-    client_auth_enabled = false
+    ssl_enabled              = true
+    client_auth_enabled      = false
     ignore_validation_errors = false
-    ciphers = []
-    protocols = []
+    ciphers                  = []
+    protocols                = []
   }
 }
 
@@ -56,8 +56,8 @@ resource "apigee_target_server" "identity-server" {
 module "personal-demographics-service" {
   source = "./modules/service"
 
-  name = "personal-demographics"
-  path = "personal-demographics"
-  env = var.env
+  name       = "personal-demographics"
+  path       = "personal-demographics"
+  env        = var.env
   proxy_type = length(regexall("sandbox", var.env)) > 0 ? "sandbox" : "live"
 }
